b2e833ee5f0e8ba9eba835dd918cfa9f5875c8f9
Igor.Sokolov <igor.sokolov@bentley.com>
2018-10-24 18:17:56 +0300

    ABDprg.params

--- jenkins/ABDprg.atp.groovy
+++ jenkins/ABDprg.atp.groovy
@@ -8,38 +8,65 @@ pipeline {
        BUILDSTRATEGY   = 'Building'
     }
 
+    parameters {
+        string(name: 'tagATP', defaultValue: '', description: 'Tag ATP before updating (black to get from tip)')
+        booleanParam(name: 'fastRun', defaultValue: false, description: 'Only run one ATP cicle (no update and iterations)')
+    }
+
     stages {        
         //**************************************************************************
         //
         stage ('bootstrap'){
-            steps{
-                bat 'bootstrap.bat BuildingIron'
+            steps {
+                script {
+                    if (!params.fastRun) {
+                        //clean Hg repos (before switch to buildingToday)
+                        bat 'bootstrap.bat BuildingIron'
+                    }
+                    else {
+                        echo 'skip bootstrap'
+                    }
+                }
             }
         }
 
         //**************************************************************************
         //
         stage ('pull ATP script'){
-            steps{
-                bat 'pullATP.bat'
+            steps {
+                script {
+                    if (!params.fastRun) {
+                        bat 'pullATP.bat'    
+                    }
+                    else {
+                        echo 'skip pull scripts'
+                    }
+                }
+                
             }
         }
 
         //**************************************************************************
         //
-        stage ('run ATP tests'){
-            steps{
-                atp false
+        stage ('run ATP tests') {
+            steps {
+                atp false, !params.fastRun, !params.fastRun, params.tagATP
             }
         }
     }
-/*   
+
     post {
         always {
-            mail  to: "${env.DEFAULT_RECIPIENTS}",
-                  subject: "${currentBuild.fullDisplayName} finished ${currentBuild.result} at ${env.BUILD_URL}",
-                  body: "Please see report ${env.BUILD_URL}"                           
+            script {
+                try {
+                    mail  to: "${env.DEFAULT_RECIPIENTS}",
+                          subject: "${currentBuild.fullDisplayName} finished ${currentBuild.result} at ${env.BUILD_URL}",
+                          body: "Please see report ${env.BUILD_URL}"                           
+                }
+                catch (exc) {
+                    echo 'Failed to send email: ' + exc
+                }
+            }
         }
     }
-  */  
 }
