8a2ae7332e34f47cbcb022947582193f98ce2ab2
Igor.Sokolov <igor.sokolov@bentley.com>
2018-11-03 14:42:45 +0300

    BF run for specified part

--- runATP.bat
+++ runATP.bat
@@ -2,4 +2,4 @@ call %~dp0sharedshell.bat
 @echo on
 
 REM ******** save first log files and clean to next run
-%SrcRoot%Building\BuildingDesigner\ATP\RunTest.bat
+%SrcRoot%Building\BuildingDesigner\ATP\RunTest.bat %1
