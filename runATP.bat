c03b51a0d68af78566a2bfaaa67fa45a55ea9fc0
Igor.Sokolov <igor.sokolov@bentley.com>
2018-11-03 17:28:17 +0300

    break on missed part

--- runATP.bat
+++ runATP.bat
@@ -3,3 +3,4 @@ call %~dp0sharedshell.bat
 
 REM ******** save first log files and clean to next run
 %SrcRoot%Building\BuildingDesigner\ATP\RunTest.bat %1
+IF NOT "%ERRORLEVEL%" == "0" EXIT %ERRORLEVEL%
