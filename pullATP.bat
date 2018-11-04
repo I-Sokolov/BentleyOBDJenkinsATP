badfc47a757597c48f17deb4888ee8b1b8b4099d
Igor.Sokolov <igor.sokolov@bentley.com>
2018-11-04 17:04:59 +0300

    pull required Hg repos

--- pullATP.bat
+++ pullATP.bat
@@ -1,5 +1,5 @@
 call %~dp0sharedshell.bat
 @echo on
 REM ----------------------------- pull scripts
-bb -r BuildingDesigner -f BuildingDesignerATP -p BuildingDesignerATP pull
+bb -r BuildingDesigner -f BuildingDesignerATP -p BuildingDesignerATP_hg pull
 
