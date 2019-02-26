0e0db632e733dadfe282de6d38eb198c7c31ad70
Igor.Sokolov <igor.sokolov@bentley.com>
2019-02-26 14:05:48 +0300

    BF resetATP cmd line

--- resetATP.bat
+++ resetATP.bat
@@ -4,8 +4,5 @@ call %~dp0sharedshell.bat
 @echo ************************* Clean ATP output **************
 @echo on
 
-#IF EXIST %OutRoot%Winx64\LogFiles\BuildingDesignerATP rd %OutRoot%Winx64\LogFiles\BuildingDesignerATP /s /q
-#IF EXIST %OutRoot%Winx64\build\triforma\atp rd  %OutRoot%Winx64\build\triforma\atp /s /q
-
 IF EXIST %OutRoot%Winx64 rd %OutRoot%Winx64 /s /q
 
