92909157768970881057d0ee1d90bd9d760ecfe9
Usama.Ajmal <usama.ajmal@bentley.com>
2021-10-05 17:37:58 +0500

    Fixing the ATP path for Jenkins Pipeline.

--- updateATP.bat
+++ updateATP.bat
@@ -7,7 +7,7 @@ if .%1==. set GETTAG=HEAD
 pushd %SrcRoot%Building\
 
 REM ----------------------------- update with subfolders and clean -C
-git pull https://bentleycs@dev.azure.com/bentleycs/Facilities%%20Engineering/_git/BuildingDev_ATPs 
+git pull https://bentleycs@dev.azure.com/bentleycs/Facilities%%20Engineering/_git/BuildingDev_ATPs atp/building/development
 IF NOT "%ERRORLEVEL%" == "0" EXIT %ERRORLEVEL%
 
 popd
