30266a9759a8de3a4ab2b710ad78e46eee26be51
Usama.Ajmal <usama.ajmal@bentley.com>
2021-10-15 16:53:28 +0500

    Add RFA repository to clone and pull commands

--- initialGetATP.bat
+++ initialGetATP.bat
@@ -8,6 +8,7 @@ pushd %SrcRoot%Building\
 
 REM ---------------------------- to create directory if missed
 IF NOT EXIST %SrcRoot%Building\atp\building\development git clone https://bentleycs@dev.azure.com/bentleycs/Facilities%%20Engineering/_git/BuildingDev_ATPs atp/building/development
+IF NOT EXIST %SrcRoot%Building\atp\building\development\tfrfa git clone https://bentleycs@dev.azure.com/bentleycs/Facilities%%20Engineering/_git/BuildingRFA_ATPs atp/building/development/tfrfa
 IF NOT "%ERRORLEVEL%" == "0" EXIT %ERRORLEVEL%
 
 popd
--- updateATP.bat
+++ updateATP.bat
@@ -8,6 +8,7 @@ pushd %SrcRoot%Building\
 
 REM ----------------------------- update with subfolders and clean -C
 git -C atp/building/development pull https://bentleycs@dev.azure.com/bentleycs/Facilities%%20Engineering/_git/BuildingDev_ATPs 
+git -C atp/building/development/tfrfa pull https://bentleycs@dev.azure.com/bentleycs/Facilities%%20Engineering/_git/BuildingRFA_ATPs
 IF NOT "%ERRORLEVEL%" == "0" EXIT %ERRORLEVEL%
 
 popd
