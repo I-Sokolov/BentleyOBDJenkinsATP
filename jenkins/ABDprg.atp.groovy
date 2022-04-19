a9ddf3d23e58b1f34bc79fe47e933002c60c8e8e
Sheetal.Sakore <sheetal.sakore@bentley.com>
2022-04-19 14:39:39 +0530

    reverted the chnages

--- jenkins/ABDprg.atp.groovy
+++ jenkins/ABDprg.atp.groovy
@@ -42,7 +42,7 @@ pipeline {
                     if (!params.fastRun) {
                         //well... clean Hg repos when switch to another team
                         //bat 'bootstrap.bat obd10_16_2'
-						bat 'bootstrap.bat building'
+						bat 'bootstrap.bat buildingazure'
                     }
                     else {
                         echo 'skip bootstrap'
