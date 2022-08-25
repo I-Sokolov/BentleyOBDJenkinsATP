008b5a74c8dc758ff245bee116469479be4a3bf2
Igor.Sokolov <igor.sokolov@bentley.com>
2022-08-25 12:20:05 +0400

    clean ATP baseline data folder when before git pull / PBI #974286: Update ATP scripts to consume Git Repos instead of HG

--- updateATP.bat
+++ updateATP.bat
@@ -6,10 +6,29 @@ if .%1==. set GETTAG=HEAD
 
 pushd %SrcRoot%Building\
 
-REM ----------------------------- update with subfolders and clean -C
-git -C atp/building/development pull https://bentleycs@dev.azure.com/bentleycs/Facilities%%20Engineering/_git/BuildingDev_ATPs 
-git -C atp/building/development/tfrfa pull https://bentleycs@dev.azure.com/bentleycs/Facilities%%20Engineering/_git/BuildingRFA_ATPs
+REM ----------------------------- update with subfolders and clean
+
+pushd atp/building/development
+dir .
+
+git reset --hard
 IF NOT "%ERRORLEVEL%" == "0" EXIT %ERRORLEVEL%
+
+git pull https://bentleycs@dev.azure.com/bentleycs/Facilities%%20Engineering/_git/BuildingDev_ATPs 
+IF NOT "%ERRORLEVEL%" == "0" EXIT %ERRORLEVEL%
+
+popd
+
+pushd atp/building/development/tfrfa
+dir .
+
+git reset --hard
+IF NOT "%ERRORLEVEL%" == "0" EXIT %ERRORLEVEL%
+
+git pull https://bentleycs@dev.azure.com/bentleycs/Facilities%%20Engineering/_git/BuildingRFA_ATPs
+IF NOT "%ERRORLEVEL%" == "0" EXIT %ERRORLEVEL%
+
+popd
 
 popd
 
