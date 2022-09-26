3cbe4569d585a11421c8ea8fd71cd3543191c1a8
Igor.Sokolov <I.Sokolov@ya.ru>
2022-09-26 14:08:37 +0400

    echo %SrcRoot%Building\BuildingDesigner\ATP\AtpHelper.py %1 %2 %3 %4 %5 %6 %7 %8 %9

--- ATPhelper.bat
+++ ATPhelper.bat
@@ -1,6 +1,6 @@
 call "%~dp0sharedshell.bat"
 
 @echo on
-
+echo %SrcRoot%Building\BuildingDesigner\ATP\AtpHelper.py %1 %2 %3 %4 %5 %6 %7 %8 %9
 %SrcRoot%Building\BuildingDesigner\ATP\AtpHelper.py %1 %2 %3 %4 %5 %6 %7 %8 %9
 
