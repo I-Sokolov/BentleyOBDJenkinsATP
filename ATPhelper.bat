8d347e958431e82af98f68147522671137dc5022
Igor.Sokolov <I.Sokolov@ya.ru>
2022-09-26 14:16:46 +0400

    fix path to AtpHelper.py / PBI #974286: Update ATP scripts to consume Git Repos instead of HG

--- ATPhelper.bat
+++ ATPhelper.bat
@@ -1,6 +1,5 @@
 call "%~dp0sharedshell.bat"
 
 @echo on
-
-%SrcRoot%Building\BuildingDesigner\ATP\AtpHelper.py %1 %2 %3 %4 %5 %6 %7 %8 %9
+%SrcRoot%Building\OpenBuildings\buildingdesigner\atp\AtpHelper.py %1 %2 %3 %4 %5 %6 %7 %8 %9
 
